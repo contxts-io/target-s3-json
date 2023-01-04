ed192d3bffe665673417f30a30d517b2f5745a70
paul <paul@contxts.io>
2023-01-04 19:30:23 +0900

    feat - fixed logic

--- target_s3_json/__init__.py
+++ target_s3_json/__init__.py
@@ -34,6 +34,7 @@ def persist_messages(messages, config, s3_client):
     key_properties = {}
     validators = {}
 
+    delimiter = config.get('delimiter', ',')
     # Use the system specific temp directory if no custom temp_dir provided
     temp_dir = os.path.expanduser(config.get('temp_dir', tempfile.gettempdir()))
 
@@ -41,13 +42,11 @@ def persist_messages(messages, config, s3_client):
     if temp_dir:
         os.makedirs(temp_dir, exist_ok=True)
 
-    filenames = []
-    file_nr = 0
-    stream = None
-    timestamp = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+    records = []
+    now = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
     logger.info(messages)
 
-    for row_count, message in enumerate(messages):
+    for message in messages:
         try:
             o = singer.parse_message(message).asdict()
         except json.decoder.JSONDecodeError:
@@ -55,24 +54,11 @@ def persist_messages(messages, config, s3_client):
             raise
         message_type = o['type']
         if message_type == 'RECORD':
-            if "__meta__" in o['stream']:
-                streamname = o['stream'][8:]
-                target_key = utils.get_target_key(o,
-                                  prefix=config.get('s3_key_prefix', ''),
-                                  timestamp=timestamp,
-                                  file_identifier=str(file_nr),
-                                  naming_convention=config.get('naming_convention_meta'))
-            else:
-                streamname = o['stream']
-                target_key = utils.get_target_key(o,
-                                  prefix=config.get('s3_key_prefix', ''),
-                                  timestamp=timestamp,
-                                  file_identifier=str(file_nr),
-                                  naming_convention=config.get('naming_convention'))
+            stream_name = o['stream']
 
-            if streamname not in schemas:
+            if stream_name not in schemas:
                 raise Exception("A record for stream {}"
-                                "was encountered before a corresponding schema".format(streamname))
+                                "was encountered before a corresponding schema".format(stream_name))
 
             # Validate record
             try:
@@ -91,22 +77,8 @@ def persist_messages(messages, config, s3_client):
             else:
                 record_to_load = utils.remove_metadata_values_from_record(o)
 
-            filename = o['stream'] + '-' + timestamp + "_" + str(file_nr) + '.json'
-            filename = os.path.expanduser(os.path.join(temp_dir, filename))
-
-            if not (filename, target_key) in filenames:
-                filenames.append((filename, target_key))
-
-            file_is_empty = (not os.path.isfile(filename)) or os.stat(filename).st_size == 0
-            flattened_record = utils.flatten_record(record_to_load)
-
-            if file_is_empty:
-                with open(filename, "w") as f:
-                    f.writelines("[")
-            with open(filename, "w") as f:
-                f.write(json.dumps(record_to_load) + config.get('delimiter', ','))
-
-            state = None
+            records.append(record_to_load)
+        # WARN - THESE CONDITIONS ARE NOT DEVELOPED
         elif message_type == 'STATE':
             logger.debug('Setting state to {}'.format(o['value']))
             state = o['value']
@@ -118,7 +90,7 @@ def persist_messages(messages, config, s3_client):
             if config.get('add_metadata_columns'):
                 schemas[stream] = utils.add_metadata_columns_to_schema(o)
 
-            stream_logname = "streams_{}_{}.log".format(config.get('edwRecordSource'), timestamp)
+            stream_logname = "streams_{}_{}.log".format(config.get('edwRecordSource'), now)
             with open(stream_logname, 'a') as streams_file:
                 streams_file.write(stream + '\n')
 
@@ -131,47 +103,46 @@ def persist_messages(messages, config, s3_client):
             logger.warning("Unknown message type {} in message {}"
                             .format(o['type'], o))
 
-        if row_count % 4000 == 0:
-            file_nr += 1
+
+    filename = os.path.expanduser(os.path.join(temp_dir, stream_name + '-' + now + '.json'))
+    target_key = utils.get_target_key(message=o,
+                                      prefix=config.get('s3_key_prefix', ''),
+                                      timestamp=now,
+                                      naming_convention=config.get('naming_convention'))
 
     # Upload to S3
-    #print(filenames)
-    for filename, target_key in filenames:
-        with open(filename, "r") as f:
-            text = json.loads(f.read() + "]")
+    with open(filename, 'w') as f:
+        f.write(json.dumps(records, indent=4))
 
-        with open(filename, 'w') as f:
-            f.write(json.dumps(text, indent=4))
-
-        compressed_file = None
-        if config.get("compression") is None or config["compression"].lower() == "none":
-            pass  # no compression
+    compressed_file = None
+    if config.get("compression") is None or config["compression"].lower() == "none":
+        pass  # no compression
+    else:
+        if config["compression"] == "gzip":
+            compressed_file = f"{filename}.gz"
+            with open(filename, 'rb') as f_in:
+                with gzip.open(compressed_file, 'wb') as f_out:
+                    logger.info(f"Compressing file as '{compressed_file}'")
+                    shutil.copyfileobj(f_in, f_out)
+            target_key += ".gz"
         else:
-            if config["compression"] == "gzip":
-                compressed_file = f"{filename}.gz"
-                with open(filename, 'rb') as f_in:
-                    with gzip.open(compressed_file, 'wb') as f_out:
-                        logger.info(f"Compressing file as '{compressed_file}'")
-                        shutil.copyfileobj(f_in, f_out)
-                target_key += ".gz"
-            else:
-                raise NotImplementedError(
-                    "Compression type '{}' is not supported. "
-                    "Expected: 'none' or 'gzip'"
-                    .format(config["compression"])
-                )
+            raise NotImplementedError(
+                "Compression type '{}' is not supported. "
+                "Expected: 'none' or 'gzip'"
+                .format(config["compression"])
+            )
 
-        s3.upload_file(compressed_file or filename,
-                       s3_client,
-                       config.get('s3_bucket'),
-                       target_key,
-                       encryption_type=config.get('encryption_type'),
-                       encryption_key=config.get('encryption_key'))
+    s3.upload_file(compressed_file or filename,
+                    s3_client,
+                    config.get('s3_bucket'),
+                    target_key,
+                    encryption_type=config.get('encryption_type'),
+                    encryption_key=config.get('encryption_key'))
 
-        # Remove the local file(s)
-        os.remove(filename)
-        if compressed_file:
-            os.remove(compressed_file)
+    # Remove the local file(s)
+    os.remove(filename)
+    if compressed_file:
+        os.remove(compressed_file)
     return state
 
 
